121e8c1d000d43a8d8f4813eddec5b9302acf293
Jechol Lee <jechol@devall.org>
2025-05-14 22:36:04 +0900

    req, req_prv, opt, opt_prv

--- lib/ash_req_opt.ex
+++ lib/ash_req_opt.ex
@@ -3,6 +3,7 @@ defmodule AshReqOpt do
     @schema Ash.Resource.Attribute.attribute_schema()
             |> Keyword.reject(fn
               {:allow_nil?, _} -> true
+              {:public?, _} -> true
               _ -> false
             end)
 
@@ -10,7 +11,7 @@ defmodule AshReqOpt do
     @req %Spark.Dsl.Entity{
       name: :req,
       describe: """
-      Declares an attribute on the resource with allow_nil?: false.
+      Declares an attribute on the resource with allow_nil?: false and public?: true.
       """,
       examples: [
         """
@@ -19,16 +20,42 @@ defmodule AshReqOpt do
         end
         """
       ],
-      transform: {__MODULE__, :req_transform, []},
+      transform: {Ash.Resource.Attribute, :transform, []},
       target: Ash.Resource.Attribute,
       args: [:name, :type],
-      schema: @schema
+      schema: @schema,
+      auto_set_fields: [
+        allow_nil?: false,
+        public?: true
+      ]
+    }
+
+    @req_prv %Spark.Dsl.Entity{
+      name: :req_prv,
+      describe: """
+      Declares an attribute on the resource with allow_nil?: false and public?: false.
+      """,
+      examples: [
+        """
+        req_prv :name, :string do
+          default "John Doe"
+        end
+        """
+      ],
+      transform: {Ash.Resource.Attribute, :transform, []},
+      target: Ash.Resource.Attribute,
+      args: [:name, :type],
+      schema: @schema,
+      auto_set_fields: [
+        allow_nil?: false,
+        public?: false
+      ]
     }
 
     @opt %Spark.Dsl.Entity{
       name: :opt,
       describe: """
-      Declares an attribute on the resource with allow_nil?: true.
+      Declares an attribute on the resource with allow_nil?: true and public?: true.
       """,
       examples: [
         """
@@ -37,38 +64,70 @@ defmodule AshReqOpt do
         end
         """
       ],
-      transform: {__MODULE__, :opt_transform, []},
+      transform: {Ash.Resource.Attribute, :transform, []},
       target: Ash.Resource.Attribute,
       args: [:name, :type],
-      schema: @schema
+      schema: @schema,
+      auto_set_fields: [
+        allow_nil?: true,
+        public?: true
+      ]
     }
 
-    def req_transform(attribute) do
-      %{attribute | allow_nil?: false} |> Ash.Resource.Attribute.transform()
+    @opt_prv %Spark.Dsl.Entity{
+      name: :opt_prv,
+      describe: """
+      Declares an attribute on the resource with allow_nil?: true and public?: false.
+      """,
+      examples: [
+        """
+        opt_prv :name, :string do
+          default nil
+        end
+        """
+      ],
+      transform: {Ash.Resource.Attribute, :transform, []},
+      target: Ash.Resource.Attribute,
+      args: [:name, :type],
+      schema: @schema,
+      auto_set_fields: [
+        allow_nil?: true,
+        public?: false
+      ]
+    }
+
+    # def transform(attribute, allow_nil?: allow_nil?, public?: public?) do
+    #   %{attribute | allow_nil?: allow_nil?, public?: public?}
+    #   |> Ash.Resource.Attribute.transform()
+    # end
+
+    def patches() do
+      [
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:attributes],
+          entity: @req
+        },
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:attributes],
+          entity: @req_prv
+        },
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:attributes],
+          entity: @opt
+        },
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:attributes],
+          entity: @opt_prv
+        }
+      ]
     end
-
-    def opt_transform(attribute) do
-      %{attribute | allow_nil?: true} |> Ash.Resource.Attribute.transform()
-    end
-
-    @add_req %Spark.Dsl.Patch.AddEntity{
-      section_path: [:attributes],
-      entity: @req
-    }
-
-    @add_opt %Spark.Dsl.Patch.AddEntity{
-      section_path: [:attributes],
-      entity: @opt
-    }
-
-    def add_req, do: @add_req
-    def add_opt, do: @add_opt
   end
 
   defmodule ReqOptBelongsTo do
     @schema Ash.Resource.Relationships.BelongsTo.opt_schema()
             |> Keyword.reject(fn
               {:allow_nil?, _} -> true
+              {:public?, _} -> true
               _ -> false
             end)
 
@@ -76,7 +135,7 @@ defmodule AshReqOpt do
     @req_belongs_to %Spark.Dsl.Entity{
       name: :req_belongs_to,
       describe: """
-      Same as belongs_to but with allow_nil?: false.
+      Same as belongs_to but with allow_nil?: false and public?: true.
       """,
       examples: [
         """
@@ -86,17 +145,39 @@ defmodule AshReqOpt do
       no_depend_modules: [:destination],
       target: Ash.Resource.Relationships.BelongsTo,
       schema: @schema,
-      transform: {__MODULE__, :transform, [false]},
+      transform: {Ash.Resource.Relationships.BelongsTo, :transform, []},
       args: [:name, :destination],
       auto_set_fields: [
-        attribute_writable?: true
+        allow_nil?: false,
+        public?: true
+      ]
+    }
+
+    @req_prv_belongs_to %Spark.Dsl.Entity{
+      name: :req_prv_belongs_to,
+      describe: """
+      Same as belongs_to but with allow_nil?: false and public?: false.
+      """,
+      examples: [
+        """
+        req_prv_belongs_to :company, Company
+        """
+      ],
+      no_depend_modules: [:destination],
+      target: Ash.Resource.Relationships.BelongsTo,
+      schema: @schema,
+      transform: {Ash.Resource.Relationships.BelongsTo, :transform, []},
+      args: [:name, :destination],
+      auto_set_fields: [
+        allow_nil?: false,
+        public?: false
       ]
     }
 
     @opt_belongs_to %Spark.Dsl.Entity{
       name: :opt_belongs_to,
       describe: """
-      Same as belongs_to but with allow_nil?: true.
+      Same as belongs_to but with allow_nil?: true and public?: true.
       """,
       examples: [
         """
@@ -106,16 +187,38 @@ defmodule AshReqOpt do
       no_depend_modules: [:destination],
       target: Ash.Resource.Relationships.BelongsTo,
       schema: @schema,
-      transform: {__MODULE__, :transform, [true]},
+      transform: {Ash.Resource.Relationships.BelongsTo, :transform, []},
       args: [:name, :destination],
       auto_set_fields: [
-        attribute_writable?: true
+        allow_nil?: true,
+        public?: true
       ]
     }
 
-    def transform(attribute, allow_nil?) do
-      %{attribute | allow_nil?: allow_nil?} |> Ash.Resource.Relationships.BelongsTo.transform()
-    end
+    @opt_prv_belongs_to %Spark.Dsl.Entity{
+      name: :opt_prv_belongs_to,
+      describe: """
+      Same as belongs_to but with allow_nil?: true and public?: false.
+      """,
+      examples: [
+        """
+        opt_prv_belongs_to :company, Company
+        """
+      ],
+      no_depend_modules: [:destination],
+      target: Ash.Resource.Relationships.BelongsTo,
+      schema: @schema,
+      transform: {Ash.Resource.Relationships.BelongsTo, :transform, []},
+      args: [:name, :destination],
+      auto_set_fields: [
+        allow_nil?: true,
+        public?: false
+      ]
+    }
+
+    # def transform(attribute, allow_nil?) do
+    #   %{attribute | allow_nil?: allow_nil?} |> Ash.Resource.Relationships.BelongsTo.transform()
+    # end
 
     @add_req %Spark.Dsl.Patch.AddEntity{
       section_path: [:relationships],
@@ -129,13 +232,28 @@ defmodule AshReqOpt do
 
     def add_req, do: @add_req
     def add_opt, do: @add_opt
+
+    def patches() do
+      [
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:relationships],
+          entity: @req_belongs_to
+        },
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:relationships],
+          entity: @req_prv_belongs_to
+        },
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:relationships],
+          entity: @opt_belongs_to
+        },
+        %Spark.Dsl.Patch.AddEntity{
+          section_path: [:relationships],
+          entity: @opt_prv_belongs_to
+        }
+      ]
+    end
   end
 
-  use Spark.Dsl.Extension,
-    dsl_patches: [
-      ReqOptAttribute.add_req(),
-      ReqOptAttribute.add_opt(),
-      ReqOptBelongsTo.add_req(),
-      ReqOptBelongsTo.add_opt()
-    ]
+  use Spark.Dsl.Extension, dsl_patches: ReqOptAttribute.patches() ++ ReqOptBelongsTo.patches()
 end
