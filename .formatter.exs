66826d6fc3c3b12515cd1bb82e490784d9319792
Jechol Lee <jechol@devall.org>
2025-05-14 23:15:58 +0900

    WIP 20250514-2315

--- .formatter.exs
+++ .formatter.exs
@@ -48,7 +48,7 @@ spark_locals_without_parens = [
 ]
 
 [
-  import_deps: [:spark, :reactor],
+  import_deps: [:spark, :reactor, :ash],
   inputs: [
     "{mix,.formatter}.exs",
     "{config,lib,test}/**/*.{ex,exs}"
--- test/ash_req_opt_test.exs
+++ test/ash_req_opt_test.exs
@@ -13,11 +13,11 @@ defmodule AshReqOptTest do
       extensions: [AshReqOpt]
 
     ets do
-      private?(true)
+      private? true
     end
 
     attributes do
-      uuid_primary_key(:id)
+      uuid_primary_key :id
     end
   end
 
@@ -28,16 +28,25 @@ defmodule AshReqOptTest do
       extensions: [AshReqOpt]
 
     ets do
-      private?(true)
+      private? true
     end
 
     attributes do
-      uuid_primary_key(:id)
+      uuid_primary_key :id
+
+      req :req, :string
+      req_prv :req_prv, :string
+
+      opt :opt, :string
+      opt_prv :opt_prv, :string
     end
 
     relationships do
-      req_belongs_to(:author, User)
-      opt_belongs_to(:reviewer, User)
+      req_belongs_to :req_user, User
+      req_prv_belongs_to :req_prv_user, User
+
+      opt_belongs_to :opt_user, User
+      opt_prv_belongs_to :opt_prv_user, User
     end
   end
 
@@ -45,18 +54,36 @@ defmodule AshReqOptTest do
     use Ash.Domain
 
     resources do
-      resource(User)
-      resource(Post)
+      resource User
+      resource Post
     end
   end
 
-  test "req, opt" do
-    assert %Attribute{allow_nil?: false} = Ash.Resource.Info.attribute(User, :email)
-    assert %Attribute{allow_nil?: true} = Ash.Resource.Info.attribute(User, :name)
+  test "req, opt, req_prv, opt_prv" do
+    assert %Attribute{allow_nil?: false, public?: true} =
+             Ash.Resource.Info.attribute(Post, :req)
+
+    assert %Attribute{allow_nil?: false, public?: false} =
+             Ash.Resource.Info.attribute(Post, :req_prv)
+
+    assert %Attribute{allow_nil?: true, public?: true} =
+             Ash.Resource.Info.attribute(Post, :opt)
+
+    assert %Attribute{allow_nil?: true, public?: false} =
+             Ash.Resource.Info.attribute(Post, :opt_prv)
   end
 
-  test "req_belongs_to, opt_belongs_to" do
-    assert %BelongsTo{allow_nil?: false} = Ash.Resource.Info.relationship(Post, :author)
-    assert %BelongsTo{allow_nil?: true} = Ash.Resource.Info.relationship(Post, :reviewer)
+  test "req_belongs_to, opt_belongs_to, req_prv_belongs_to, opt_prv_belongs_to" do
+    assert %BelongsTo{allow_nil?: false, public?: true} =
+             Ash.Resource.Info.relationship(Post, :req_user)
+
+    assert %BelongsTo{allow_nil?: false, public?: false} =
+             Ash.Resource.Info.relationship(Post, :req_prv_user)
+
+    assert %BelongsTo{allow_nil?: true, public?: true} =
+             Ash.Resource.Info.relationship(Post, :opt_user)
+
+    assert %BelongsTo{allow_nil?: true, public?: false} =
+             Ash.Resource.Info.relationship(Post, :opt_prv_user)
   end
 end
