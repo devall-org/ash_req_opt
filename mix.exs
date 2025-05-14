57db8e25744706b380e059519178d30a143ba7c1
Jechol Lee <jechol@devall.org>
2025-05-14 23:09:29 +0900

    spark.formatter

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule AshReqOpt.MixProject do
   defp deps do
     [
       {:ash, ">= 0.0.0"},
-      {:sourceror, ">= 0.0.0", only: [:dev, :test]},
+      {:spark, ">= 0.0.0"},
+      {:sourceror, ">= 0.0.0", only: [:dev, :test], optional: true},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
